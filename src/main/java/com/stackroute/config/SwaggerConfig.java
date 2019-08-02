718e6414a5b36805cb3fb335bf567fde5139c117
sneha <sneha.meshram@cgi.com>
2019-08-02 13:08:58 +0530

    add comments for SwaggerConfig class

--- src/main/java/com/stackroute/config/SwaggerConfig.java
+++ src/main/java/com/stackroute/config/SwaggerConfig.java
@@ -1,8 +1,19 @@
 package com.stackroute.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
+    //Generate API documentation using Swagger 2
+
     @Bean
+    public Docket productApi(){
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
 }
