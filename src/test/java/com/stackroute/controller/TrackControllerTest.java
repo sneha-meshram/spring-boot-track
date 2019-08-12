43b102315dd2f5f78164d7bad22912b8db987a27
sneha <sneha.meshram@cgi.com>
2019-08-12 19:11:35 +0530

    track-service-testcases for method commit

--- src/test/java/com/stackroute/controller/TrackControllerTest.java
+++ src/test/java/com/stackroute/controller/TrackControllerTest.java
@@ -1,8 +1,140 @@
 package com.stackroute.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.stackroute.domain.Track;
+import com.stackroute.exceptions.GlobalException;
+import com.stackroute.exceptions.TrackAlreadyExistsException;
+import com.stackroute.exceptions.TrackNotFound;
+import com.stackroute.repository.TrackRepository;
+import com.stackroute.service.TrackService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.verification.Times;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static jdk.internal.vm.compiler.word.LocationIdentity.any;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class TrackControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private Track track;
+    @MockBean
+    private TrackService trackService;
+    @InjectMocks
+    private TrackController trackController;
 
+    private List<Track> list = null;
+    private TrackRepository trackRepository;
 
+    @Before
+    public void setUp() {
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(trackController).setControllerAdvice(new GlobalException()).build();
+//        mockMvc = MockMvcBuilders.standaloneSetup(trackController).build();
+        track = new Track();
+        track.setId(10);
+        track.setName("Baby");
+        track.setComment("Baby Song");
+        list = new ArrayList();
+        list.add(track);
+    }
+
+    @After
+    public void tearDown() {
+        list = null;
+        track = null;
+    }
+
+    //testcase for success of save method.
+    @Test
+    public void givenTrackShouldReturnsaveTrack() throws Exception {
+        when(trackService.save(any())).thenReturn(track);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/track")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
+        verify(trackRepository, times(1)).getAllTrack(track);
+    }
+
+    //testcase for failure of save method.
+    @Test
+    public void givenTrackShouldReturnsaveTrackFailure() throws Exception {
+        when(trackService.save(any())).thenThrow(TrackAlreadyExistsException.class);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/track")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isConflict())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    //testcase for getAllTrack method.
+    @Test
+    public void givenTrackShouldReturngetAllTrack() throws Exception {
+        when(trackService.getAllTrack()).thenReturn(list);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/track")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+        verify(trackRepository, times(1)).getAllTrack(track);
+    }
+
+    //testcase for UpdateById method.
+    @Test
+    public void givenTrackShouldReturnUpdatedTrack() throws Exception {
+        when(trackService.updateTrackById(anyInt())).thenReturn(track);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/track")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+        verify(trackRepository, times(1)).getAllTrack(track);
+    }
+
+    //testcase for UpdateTrackById
+    @Test
+    public void givenTrackShouldReturnTrackNotFoundException() throws Exception {
+        when(trackService.updateTrackById(anyInt())).thenThrow(TrackNotFound.class);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/track")
+                .contentType(MediaType.APPLICATION_JSON).content(asJsonString(track)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    //testcase for deleteTrackById method
+    @Test
+    public void givenTrackIdShouldDeleteTrackAndReturnDeletedTrack() throws Exception {
+        when(trackService.deleteTrackById(anyInt())).thenReturn(Optional.of(track));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/track?id=20")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isMovedPermanently())
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
