f1d12bc540b5aa881dc3b15f4ad2ad2d9614227e
sneha <sneha.meshram@cgi.com>
2019-08-12 12:57:52 +0530

    track-service-dev and prod properties commit

--- src/main/java/com/stackroute/TrackServiceApplication.java
+++ src/main/java/com/stackroute/TrackServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("application.properties")
+@PropertySource("application-prod.properties")
 @SpringBootApplication
 public class TrackServiceApplication {
 
